0b27b64743796862ac743ee544f78783bbfd31bc
out <outline941@live.com>
2020-04-30 23:33:00 +0800

    add nmap script help

--- nmap.sh
+++ nmap.sh
@@ -1,1 +1,7 @@
-nmap --script ./iptv-eds.nse --max-hostgroup 40 -n -sn --open -PS8082 $@
+#!/bin/sh
+if [ $1 = '-h' ] || [ $1 = '--help' ]; then
+
+	echo "usage: $0 [nmap option] <target>"
+else
+	nmap --script ./iptv-eds.nse --max-hostgroup 40 -n -sn --open -PS8082 $@
+fi
